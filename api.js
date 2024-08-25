d1d1b99b4b8fc1500a3946ff9f09e7d31475b0fb
hyun907 <seunghyun020907@naver.com>
2024-08-25 19:29:12 +0900

    feat: 페이지네이션 구현

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
-export async function getReviews(order = "createdAt") {
-  const query = `order=${order}`;
-  const response = await fetch(`https://learn.codeit.kr/4651/film-reviews?${query}`);
+export async function getReviews({order = "createdAt", offset = 0, limit = 6}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/4651/film-reviews?${query}`
+  );
   const body = await response.json();
   return body;
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useEffect, useState } from "react";
 import ReviewList from "./components/ReviewList";
 import { getReviews } from "../api";
 
+const LIMIT = 6;
+
 export default function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNest] = useState(false);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,13 +20,23 @@ export default function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getReviews(orderQuery);
-    setItems(reviews);
+  const handleLoad = async (options) => {
+    const { paging, reviews } = await getReviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setOffset(options.offset + options.limit);
+    setHasNest(paging.hasNext);
+  };
+
+  const handleLoadMore = async () => {
+    await handleLoad({ order, offset, limit: LIMIT });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: LIMIT });
   }, [order]);
 
   return (
@@ -32,6 +46,7 @@ export default function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelte} />
+      {hasNext && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
